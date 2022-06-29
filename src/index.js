6983e523ec09dc463b6019fc3430b4a6d899723b
Jiny <jiny8510@gmail.com>
2022-06-29 12:21:17 +0900

    #6 [2021 UPDATE] EFFECTS-Recap

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode> 두번 렌더링 됨
 );
