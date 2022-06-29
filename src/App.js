ed6f936dfd769b7cf9ce8d6567da989426e5a135
Jiny <jiny8510@gmail.com>
2022-06-29 16:28:08 +0900

    #6 [2021 UPDATE] EFFECTS-Cleanup

--- src/App.js
+++ src/App.js
@@ -2,32 +2,21 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []); //useEffect의 return은 unmount시에만 동작함
+  return <h1>Hello</h1>;
+}
 
-  const iRunOnlyOnce = () => {
-    console.log("I run only once.");
-  };
-  useEffect(iRunOnlyOnce, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
