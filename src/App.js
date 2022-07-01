7b9cb80b25bed01fa05052f5ed9c5678798f5981
Jiny <jiny8510@gmail.com>
2022-07-01 17:43:04 +0900

    Router basename 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./routes/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/movie/:id">
           <Detail />
