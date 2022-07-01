486276f6a5c6c1790739d35018539b05c8f6dfeb
Jiny <jiny8510@gmail.com>
2022-07-01 17:52:12 +0900

    Router basename 수정3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/movie/:id">
+        <Route basename={process.env.PUBLIC_URL} path="/movie/:id">
           <Detail />
         </Route>
         <Route basename={process.env.PUBLIC_URL} path="/">
